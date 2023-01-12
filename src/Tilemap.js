d30aee333e687c23d7fbc9c3c9691edd64b56b56
KEVN <troostkevin8@gmail.com>
2023-01-12 18:45:25 +0100

    update game

--- src/Tilemap.js
+++ src/Tilemap.js
@@ -1,6 +1,8 @@
-export default class TileMap{
-    constructor(){}
-
+export default class TileMap {
+    constructor(tileSize) {
+        this.tileSize = tileSize;
+    }
+    
     draw() {
         console.log("draw");
     }
--- src/game.js
+++ src/game.js
@@ -1,9 +1,13 @@
+import TileMap from './TileMap.js';
+
+const tileSize = 32;
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
+const tileMap = new TileMap(10, 10, 64);
 
 function gameLoop() {
-  console.log("game loop");
-  
+  tileMap
+
 }
 
-setInterval(gameLoop, 1000/75);
+setInterval(gameLoop, 1000 / 75);
