f2b7617c869aaa7c4a474769fdf9225720aab10b
KEVN <troostkevin8@gmail.com>
2023-01-16 13:28:07 +0100

    Update Character.js

--- src/Character.js
+++ src/Character.js
@@ -53,27 +53,27 @@ export default class Pacman {
     #keydown = (event) => {
         //up
         if (event.keyCode == 38) {
-            if (this.currentMovingDirection == movingDirection.down)
-                this.currentMovingDirection = movingDirection.up;
-            this.requestedMovingDirection = movingDirection.up;
+            if (this.currentMovingDirection == MovingDirection.down)
+                this.currentMovingDirection = MovingDirection.up;
+            this.requestedMovingDirection = MovingDirection.up;
         }
         //down
         if (event.keyCode == 40) {
-            if (this.currentMovingDirection == movingDirection.up)
-                this.currentMovingDirection = movingDirection.down;
-            this.requestedMovingDirection = movingDirection.down;
+            if (this.currentMovingDirection == MovingDirection.up)
+                this.currentMovingDirection = MovingDirection.down;
+            this.requestedMovingDirection = MovingDirection.down;
         }
         //left
         if (event.keyCode == 37) {
-            if (this.currentMovingDirection == movingDirection.right)
-                this.currentMovingDirection = movingDirection.left;
-            this.requestedMovingDirection = movingDirection.left;
+            if (this.currentMovingDirection == MovingDirection.right)
+                this.currentMovingDirection = MovingDirection.left;
+            this.requestedMovingDirection = MovingDirection.left;
         }
         //right
         if (event.keyCode == 39) {
-            if (this.currentMovingDirection == movingDirection.left)
-                this.currentMovingDirection = movingDirection.right;
-            this.requestedMovingDirection = movingDirection.right;
+            if (this.currentMovingDirection == MovingDirection.left)
+                this.currentMovingDirection = MovingDirection.right;
+            this.requestedMovingDirection = MovingDirection.right;
         }
     };
     #move(){
